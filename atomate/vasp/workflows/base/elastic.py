9bffe684d0816f061a2e2e8724f178145c80a4d7
montoyjh <montoyjh@gmail.com>
2017-03-27 19:10:17 -0700

    new commit for strain refactor

--- atomate/vasp/workflows/base/elastic.py
+++ atomate/vasp/workflows/base/elastic.py
@@ -24,7 +24,7 @@ __email__ = 'shyamd@lbl.gov, montoyjh@lbl.gov'
 logger = get_logger(__name__)
 
 
-def get_wf_elastic_constant(structure, norm_deformations=None, shear_deformations=None, 
+def get_wf_elastic_constant(structure, norm_deformations=[0.01], shear_deformations=[0.03], 
                             db_file=None, conventional=True, **kwargs):
     """
     Returns a workflow to calculate elastic constants.
@@ -32,7 +32,7 @@ def get_wf_elastic_constant(structure, norm_deformations=None, shear_deformation
     Firework 1 : write vasp input set for structural relaxation,
                  run vasp,
                  pass run location,
-                 database insertion.
+                 database insertion
 
     Firework 2 - number of total deformations: Static runs on the deformed structures
     
@@ -44,7 +44,7 @@ def get_wf_elastic_constant(structure, norm_deformations=None, shear_deformation
         shear_deformations (list of floats): list of values to for shear deformations.
         db_file (str): path to file containing the database credentials.
         kwargs (keyword arguments): additional kwargs to be passed to get_wf_deformations
-            e. g. "vasp_cmd" etc.
+            e. g. vasp_cmd, user_kpoints_density, optimize_structure
 
     Returns:
         Workflow
@@ -54,25 +54,17 @@ def get_wf_elastic_constant(structure, norm_deformations=None, shear_deformation
         structure = SpacegroupAnalyzer(structure).get_conventional_standard_structure()
 
     # Generate deformations from normal and shear values provided
-    deformations = []
-    if norm_deformations is not None:
-        for nd, ind in itertools.product(norm_deformations, range(3)):
-            deformations.append(Deformation.from_index_amount(vmap[ind], nd))
-    if shear_deformations is not None:
-        for sd, ind in itertools.product(shear_deformations, range(3,6)):
-            deformations.append(Deformation.from_index_amount(vmap[ind], sd))
+    strains = []
+    for nd in norm_deformations:
+        strains.extend(np.eye(6)[:3]*nd)
+    for sd in shear_deformations:
+        strains.extend(np.eye(6)[3:]*sd)
 
-    if additional_deformations:
-        deformations.extend([Deformation(d) for d in additional_deformations])
-
-    strain_mat = [d.green_lagrange_strain.voigt for d in deformations]
-    if not deformations or np.linalg.matrix_rank(np.array(strain_mat)) < 6:
-        raise ValueError("Strain list is insufficient to fit an elastic tensor")
-
-    wf_elastic = get_wf_deformations(structure, deformations, pass_stress_strain=True, 
+    defos = [Strain.from_voigt(strain).deformation_matrix for strain in strains]
+    wf_elastic = get_wf_deformations(structure, defos, pass_stress_strain=True, 
             name="deformation", relax_deformed=True, tag="elastic", **kwargs)
 
-    fw_analysis = Firework(ElasticTensorToDbTask(structure=structure, db_file=db_file, toec=toec),
+    fw_analysis = Firework(ElasticTensorToDbTask(structure=structure, db_file=db_file),
                            name="Analyze Elastic Data", spec={"_allow_fizzled_parents": True})
     append_fw_wf(wf_elastic, fw_analysis)
 
@@ -81,10 +73,10 @@ def get_wf_elastic_constant(structure, norm_deformations=None, shear_deformation
     return wf_elastic
 
 
-def get_wf_toec(structure, max_strain=0.05, stencil_res=7, indices=None, **kwargs):
+def get_wf_elastic_toec(structure, max_strain=0.05, stencil_res=5, indices=None, **kwargs):
     """
     Returns a workflow to calculate third-order elastic constants.
-
+    
     Args:
         structure (Structure): input structure to be optimized and run. 
         max_strain (float): maximum strain
@@ -96,7 +88,7 @@ def get_wf_toec(structure, max_strain=0.05, stencil_res=7, indices=None, **kwarg
         Workflow
     """
     if stencil_res % 2 != 1 or stencil_res < 5:
-        raise ValueError("Stencil resolution for TOECs must be an odd integer greater than 5")
+        raise ValueError("Stencil resolution must be an odd integer greater than 5")
 
     # Generate deformations
     default_ind = [(i) for i in range(6)] + [(0, i) for i in range(1, 5)] \
